2da822511318a0db85de82ff4e14040e91b2597e
ale <alex1313shi@gmail.com>
2025-05-19 20:02:41 -0600

    agregar html

---
+++ src/app/nabvar/nabvar/nabvar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  imports: [],
+  templateUrl: './nabvar.component.html',
+  styleUrl: './nabvar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NabvarComponent { }
